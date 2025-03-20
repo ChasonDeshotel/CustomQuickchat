079ca48f45e2b15300031fab81994223a6a03213
Chason Deshotel <me@chasondeshotel.com>
2025-03-20 00:42:58 -0500

    wip

--- src/game/ChatManager.cpp
+++ src/game/ChatManager.cpp
@@ -1,6 +1,17 @@
 #include "ChatManager.h"
 
 void
+Chat::initHooks() {
+    gameWrapper->HookEventWithCaller<ActorWrapper>("Function TAGame.GFxData_Chat_TA.OnChatMessage",
+        [this](ActorWrapper caller, void* params, std::string eventName) { this->onChatMessage(caller, params, eventName); });
+
+    gameWrapper->HookEventWithCaller<ActorWrapper>("Function TAGame.GFxData_Chat_TA.OnPressChatPreset",
+        [this](ActorWrapper caller, void* params, std::string eventName) { this->onPressChatPreset(caller, params, eventName); });
+
+    // Other chat-related hooks...
+}
+
+void
 ChatManager::SendChat(const std::string& chat, EChatChannel chatMode) {
     auto enabledCvar = GetCvar(Cvars::enabled);
     if (chat.empty() || !enabledCvar || !enabledCvar.getBoolValue()) {
@@ -289,4 +300,49 @@ ChatManager::apply_custom_qc_labels_to_ui(UGFxData_Chat_TA* caller, UGFxData_Cha
 void
 CustomQuickchat::ResetChatTimeoutMsg() {
     chatTimeoutMsg = "Chat disabled for [Time] second(s).";
+}
+
+void
+Chat::SetTimeoutMsg(const std::string& newMsg, AGFxHUD_TA* hud) {
+    if (!hud) {
+        hud = GetInstanceOf<AGFxHUD_TA>();
+        if (!hud)
+            return;
+    }
+
+    if (hud->ChatDisabledMessage.ToString() != newMsg) {
+        hud->ChatDisabledMessage = Instances.NewFString(newMsg); // overwrite ChatDisabledMessage
+        LOG("Set chat timeout message: {}", newMsg);
+    }
+}
+
+void
+Chat::SendChat(const std::string& chat, EChatChannel chatMode, bool log) {
+    UGFxData_Chat_TA* chatBox = GetInstanceOf<UGFxData_Chat_TA>();
+    if (!chatBox) {
+        LOG("UGFxData_Chat_TA* is null!");
+        return;
+    }
+
+    FString chatFStr = Instances.NewFString(chat);
+
+    if (chatMode == EChatChannel::EChatChannel_Match) {
+        chatBox->SendChatMessage(chatFStr, 0); // match (lobby) chat
+
+        if (log) {
+            LOG("Sent chat: '{}'", chat);
+        }
+    } else if (chatMode == EChatChannel::EChatChannel_Team) {
+        chatBox->SendTeamChatMessage(chatFStr, 0); // team chat
+
+        if (log) {
+            LOG("Sent chat: [Team] '{}'", chat);
+        }
+    } else if (chatMode == EChatChannel::EChatChannel_Party) {
+        chatBox->SendPartyChatMessage(chatFStr, 0); // party chat
+
+        if (log) {
+            LOG("Sent chat: [Party] '{}'", chat);
+        }
+    }
 }
